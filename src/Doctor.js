e6f1603a0713727dcde01447418b8c1e51ee29eb
Dalia Ramos <dalia.ramoshernandez@gmail.com>
2018-03-23 16:56:22 -0700

    add to main.js

--- src/Doctor.js
+++ src/Doctor.js
@@ -1,8 +1,9 @@
-export class DoctorSearch{
+export class doctorSearch{
 
-  DS(firstName, lastName, location){
+  DS(){
     return new promise(function(resolve, reject){
       let request = new XMLHTTPRequest();
+      let location = "PDX";
       let url =  `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=PDX&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
       request.onload = function(){
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { doctorSearch } from './Doctor.js'
 //import { **insert prototype name** } from './Doctor.js';
 
 $(document).ready(function() {
   $("searchDoctor").submit(function(event){
     event.preventDefault();
+
+    let newDoctor = new doctorSearch();
+    let promise = newDoctor.DS();
+    promise.then(function(event){
+
+    });
+    // promise.then(function(response){
+    //   let body = JSON.parse(response);
+    //   let array = [];
+    //   body.
+    })
   });
 });
