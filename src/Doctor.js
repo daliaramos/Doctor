d818bc5914dbe4ef51ffd8005f9ff9c815add9ae
Dalia Ramos <dalia.ramoshernandez@gmail.com>
2018-03-26 20:09:12 -0700

    add to main.js.

--- src/Doctor.js
+++ src/Doctor.js
@@ -1,22 +1,15 @@
-export class doctorSearch{
-  constructor(disease){
-    this.disease = disease;
-  }
+import $ from 'jquery';
 
-  DS(disease){
-    return new promise(function(resolve, reject){
-      let request = new XMLHTTPRequest();
-      let url =  `https://api.betterdoctor.com/2016-03-01/doctors?query=${disease}&location=PDX&user_location=45.5231%2C%20122.6765&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+export class doctorSearch {
 
-      request.onload = function(){
-        if(this.status === 200){
-          resolve(request.response);
-        }else{
-          reject(ERROR(request.statusText));
-        }
-      }
-      request.open("GET",  url, true);
-      request.send();
+  getDS(disease, drSearch) {
+
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${disease}&location=or-portland&gender=female&skip=0&limit=10&user_key=ed2ecce59160c9536487ba1383974f35`)
+    .then(response => {
+      drSearch(response);
+    })
+    .fail(() => {
+      $('#error').text("ERROR")
     });
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { doctorSearch } from './dDctor.js';
+import { doctorSearch } from './doctor.js';
 
-const DS = response => {
+const drSearch = response => {
   if (response.data.length == 0 ) {
-    $('#errors').text("ERROR")
+    $('.errors').text("ERROR")
   } else {
     response.data.forEach(data => {
       let firstName = data.profile.first_name;
-      // let lastName = data.profile.last_name;
-
-
-  })
+      let lastName = data.profile.last_name;
+      let street = data.practices[0].visit_address.street;
+      let state = data.practices[0].visit_address.state;
+      let gender = "female";
+    $('#viewDr').append(`<p>`+ firstName + " "  + `</p>`
+                                + `<p>` + lastName + " " + `</p>`
+                                    + `<p>`  + street + `</p>`
+                                    + `<p>` + state + " "+ `</p>`
+                                  + `<p>` + gender + " " + `</p>`)
+    })
   }
 }
+
+$(document).ready(function() {
+  $('.find').click(e => {
+  e.preventDefault();
+  let findDoc = $('#doc').val();
+  doctorSearch.prototype.getDS(findDoc, drSearch)
+  $('#viewDr').empty();
+  });
+});
